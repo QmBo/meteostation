4f93c4da11e1da6eacf70c726b4bf95a2e31b768
Victor Egorov <qrioflat@gmail.com>
2021-03-12 17:47:47 +0300

    First Commit

---
+++ src/main/java/ru/qmbo/meteostation/MeteostationApplication.java
@@ -0,0 +1,13 @@
+package ru.qmbo.meteostation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteostationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteostationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/qmbo/meteostation/MeteostationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.qmbo.meteostation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteostationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
